081cc3c5fe9bf20be5442bda5b8d5993e4e88024
Firstname Lastname <enzoesper7@gmail.com>
2024-01-29 02:05:43 -0300

    adding an android ripple effect for feedback when pressed and also an ios alternative

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,22 +3,29 @@ import React from "react";
 
 export default function GoalItem({ text, onDeleteItem, id }) {
   return (
-    <Pressable onPress={onDeleteItem.bind(this, id)}>
-      <View style={styles.goalItemContainer}>
+    <View style={styles.goalItemContainer}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={onDeleteItem.bind(this, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalItemText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItemContainer: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc"
   },
   goalItemText: {
-    color: "white"
+    color: "white",
+    padding: 8
+  },
+  pressedItem: {
+    opacity: 0.5
   }
 });
